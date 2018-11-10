c256467ae5fc870d1ea8c9fb700da385fc695a86
blerka <blerka@blerkas-iMac.local>
2018-11-10 23:25:51 +0300

    init commit

---
+++ hardCPP/Test.cpp
@@ -0,0 +1,8 @@
+//
+//  Test.cpp
+//  
+//
+//  Created by blerka on 8/3/18.
+//
+
+#include "Test.hpp"
---
+++ hardCPP/Test.hpp
@@ -0,0 +1,13 @@
+//
+//  Test.hpp
+//  
+//
+//  Created by blerka on 8/3/18.
+//
+
+#ifndef Test_hpp
+#define Test_hpp
+
+#include <stdio.h>
+
+#endif /* Test_hpp */
